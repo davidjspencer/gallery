f0595968106afc42077587af011c96f37dff722a
davidjspencer <davidjspencer@gmail.com>
2018-03-01 16:21:22 +0100

    animation js delay

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+function animation() {
+  var circle = document.getElementById('circle');
+  var scribble = document.getElementById('scribble');
+
+  circle.classList.add('circle-animation');
+  scribble.classList.add('scribble-animation');
+  console.log('hello');
+}
